15db36a7c5710a8a811fe70733f77aa02023f194
Reidar Djupvik <mail@3rdhand.info>
2018-12-14 20:47:47 +0000

    Configure babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  module: { rules: [{ test: /\.js$/, use: 'babel-loader' }] },
   plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })]
 };
