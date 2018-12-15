ddd6fd286b7d1775f7d578f9b0fb6da4f7188210
Reidar Djupvik <mail@3rdhand.info>
2018-12-15 23:32:01 +0000

    Add both entry paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: [
+    path.resolve(__dirname, 'src/index.js'),
+    path.resolve(__dirname, 'src/telefon.js')
+  ],
   module: { rules: [{ test: /\.js$/, use: 'babel-loader' }] },
   plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })]
 };
